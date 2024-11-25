dede84277e8b26d04e70fd2cb4ab9c7536e03aac
Olim8 <141115970+Olim8@users.noreply.github.com>
2024-11-25 09:39:31 +0500

    Delete library_app directory

--- library_app/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.generics import ListAPIView
-from books.models import Book
-from .serializers import BookSerializer
-
-class BookApiView(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
